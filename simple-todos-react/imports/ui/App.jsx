e1e419bf765ca94226591966f904f02764384174
Gabriel Saturnino <gabrielsaturnino4@gmail.com>
2023-04-27 21:29:53 -0300

    criando tasks

--- simple-todos-react/imports/ui/App.jsx
+++ simple-todos-react/imports/ui/App.jsx
@@ -2,13 +2,18 @@ import React from 'react';
 import { Task } from './Task';
 import { useTracker } from 'meteor/react-meteor-data';
 import { TasksCollection } from '../api/TasksCollection';
+import { TaskForm } from './TaskForm';
 
 export const App = () => {
-  const tasks = useTracker(() => TasksCollection.find({}).fetch());
+  const tasks = useTracker(() => TasksCollection.find({},
+    { sort: { createdAt: -1 } }).fetch());
 
   return (
     <div>
       <h1>Welcome to Meteor!</h1>
+
+      <TaskForm />
+
       <ul>
         {tasks.map(task => <Task key={task._id} task={task} />)}
       </ul>
