549a49d7941a5e21305e18162356fbb4b280d7c8
Gabriel Saturnino <gabrielsaturnino4@gmail.com>
2023-04-27 21:03:18 -0300

    Collections

---
+++ simple-todos-react/imports/ui/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Task } from './Task';
+import { useTracker } from 'meteor/react-meteor-data';
+import { TasksCollection } from '../api/TasksCollection';
+
+export const App = () => {
+  const tasks = useTracker(() => TasksCollection.find({}).fetch());
+
+  return (
+    <div>
+      <h1>Welcome to Meteor!</h1>
+      <ul>
+        {tasks.map(task => <Task key={task._id} task={task} />)}
+      </ul>
+    </div>
+  );
+};
